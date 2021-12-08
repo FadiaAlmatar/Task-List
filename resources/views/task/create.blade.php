12312d9bee992e980048e4289a48c14c522e3bcf
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-12-08 20:16:05 +0200

    fix

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -52,11 +52,10 @@
                 <p class="help is-danger" style="color: red">{{ $message }}</p>
             @enderror
         </div>
-        <br><br><br>
+
                 <button type="submit" class="btn btn-success"> <i class="fa fa-check"></i> {{__('Save')}}</button>
                 <a href="{{route('dashboard')}}"><button type="button" class="btn btn-danger">{{__('Cancel')}}</button></a>
         </form>
-        <br>
     </div>
 @endsection
 
